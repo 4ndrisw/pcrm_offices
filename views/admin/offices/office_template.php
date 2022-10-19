98fd7f150f18bfb3c9a793efce489f8f49b9de92
4ndr1sw <4ndr1sw@gmail.com>
2022-10-19 10:35:16 +0700

    Update office_template.php

--- views/admin/offices/office_template.php
+++ views/admin/offices/office_template.php
@@ -39,8 +39,8 @@
             <?php $value = (isset($office) ? $office->nip_pengawas_ipk : ''); ?>
             <?php echo render_input('nip_pengawas_ipk','nip_pengawas_ipk',$value); ?>
 
-            <?php $value = (isset($office) ? $office->nama_pengawas_ipka : ''); ?>
-            <?php echo render_input('nama_pengawas_ipka','nama_pengawas_ipka',$value); ?>
+            <?php //$value = (isset($office) ? $office->nama_pengawas_ipka : ''); ?>
+            <?php //echo render_input('nama_pengawas_ipka','nama_pengawas_ipka',$value); ?>
             <?php $value = (isset($office) ? $office->nip_pengawas_ipka : ''); ?>
             <?php echo render_input('nip_pengawas_ipka','nip_pengawas_ipka',$value); ?>
 
